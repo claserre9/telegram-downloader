298a342b7a9e6ee550980f9791c808502a34178b
Clifford Laserre <claserre9@gmail.com>
2025-03-21 09:22:58 -0400

    Refactor Telegram client integration and update dependencies.
    Replaced MadelineProto with a custom TelegramClient implementation for improved manageability. Introduced phpstan configuration and updated Composer dependencies to their latest versions, enhancing code quality and compatibility. Removed unnecessary exception handling from download.php for better clarity.

--- src/download.php
+++ src/download.php
@@ -1,8 +1,6 @@
 <?php
 
-use danog\MadelineProto\API;
-use danog\MadelineProto\Settings\AppInfo;
-use danog\MadelineProto\Settings\Logger;
+use claserre9\TelegramClient;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -13,74 +11,66 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
-try {
-    $input = new ArgvInput();
-    $output = new ConsoleOutput();
-    $io = new SymfonyStyle($input, $output);
 
-    if(!isset($_ENV['TELEGRAM_API_ID'], $_ENV['TELEGRAM_API_HASH'], $_ENV['TELEGRAM_CHANNEL'])){
-        $io->error('TELEGRAM_CHANNEL, TELEGRAM_API_ID and TELEGRAM_API_HASH env are not set');
-        throw new RuntimeException('TELEGRAM_CHANNEL, TELEGRAM_API_ID and TELEGRAM_API_HASH env are not set');
+$input = new ArgvInput();
+$output = new ConsoleOutput();
+$io = new SymfonyStyle($input, $output);
+
+if (!isset($_ENV['TELEGRAM_API_ID'], $_ENV['TELEGRAM_API_HASH'], $_ENV['TELEGRAM_CHANNEL'])) {
+    $io->error('TELEGRAM_CHANNEL, TELEGRAM_API_ID and TELEGRAM_API_HASH env are not set');
+    throw new RuntimeException('TELEGRAM_CHANNEL, TELEGRAM_API_ID and TELEGRAM_API_HASH env are not set');
+}
+
+
+$limit = 100;
+$lastMessageId = 0;
+
+$client = new TelegramClient(
+    $_ENV['TELEGRAM_API_ID'], 
+    $_ENV['TELEGRAM_API_HASH']);
+$client->start();
+
+$MadelineProto = $client->getMadeline();
+
+$downloadPath = __DIR__ . '/../downloads/';
+
+$fileSystem = new Filesystem();
+if (!$fileSystem->exists($downloadPath)) {
+    $io->comment("Creating download directory: $downloadPath");
+    $fileSystem->mkdir($downloadPath);
+}
+
+do {
+    $messages = $MadelineProto->messages->getHistory([
+        'peer' => $_ENV['TELEGRAM_CHANNEL'],
+        'offset_id' => $lastMessageId,
+        'limit' => $limit,
+        'add_offset' => 0,
+        'max_id' => 0,
+        'min_id' => 0,
+        'hash' => 0
+    ]);
+
+    if (empty($messages['messages'])) {
+        $io->comment('No messages found.');
+        break;
     }
 
-    $limit = 100;
-    $lastMessageId = 0;
+    foreach ($messages['messages'] as $message) {
+        $lastMessageId = $message['id'];
 
-    $settings = (new AppInfo())
-        ->setApiId($_ENV['TELEGRAM_API_ID'])
-        ->setApiHash($_ENV['TELEGRAM_API_HASH']);
+        if (isset($message['media'])) {
+            $mediaType = $message['media']['_'];
 
-    $loggerSettings = (new Logger)
-        ->setLevel(\danog\MadelineProto\Logger::LEVEL_WARNING);
-
-    $MadelineProto = new API('session.madeline', $settings);
-    $MadelineProto->updateSettings($loggerSettings);
-
-    $MadelineProto->start();
-
-    $downloadPath = __DIR__ . '/../downloads/';
-
-    $fileSystem = new Filesystem();
-    if (!$fileSystem->exists($downloadPath)) {
-        $io->comment("Creating download directory: $downloadPath");
-        $fileSystem->mkdir($downloadPath);
-    }
-
-    do {
-        $messages = $MadelineProto->messages->getHistory([
-            'peer' => $_ENV['TELEGRAM_CHANNEL'],
-            'offset_id' => $lastMessageId,
-            'limit' => $limit,
-            'add_offset' => 0,
-            'max_id' => 0,
-            'min_id' => 0,
-            'hash' => 0
-        ]);
-
-        if (empty($messages['messages'])) {
-            $io->comment('No more messages');
-            break;
-        }
-
-        foreach ($messages['messages'] as $message) {
-            $lastMessageId = $message['id'];
-
-            if (isset($message['media'])) {
-                $mediaType = $message['media']['_'];
-
-                if (in_array($mediaType, ['messageMediaPhoto', 'messageMediaDocument'])) {
-                    try {
-                        $file = $MadelineProto->downloadToDir($message['media'], $downloadPath);
-                        $io->info("Downloaded: $file");
-                    } catch (Exception $e) {
-                        $io->error("Download error: $e");
-                    }
+            if (in_array($mediaType, ['messageMediaPhoto', 'messageMediaDocument'])) {
+                try {
+                    $file = $MadelineProto->downloadToDir($message['media'], $downloadPath);
+                    $io->info("Downloaded: $file");
+                } catch (Exception $e) {
+                    $io->error("Download error: $e");
                 }
             }
         }
+    }
 
-    } while (count($messages['messages']) >= $limit);
-
-} catch (\danog\MadelineProto\Exception $e) {
-    echo 'MadelineProto error: ' . $e->getMessage();
-}
+} while (count($messages['messages']) >= $limit);
